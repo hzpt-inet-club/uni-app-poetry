233fba805d21b5c57b07de639b7c5bc6bcd4ece2
xiaoxunyao <huchengyea@163.com>
2021-08-25 20:30:20 +0800

    今日晚间的工作

---
+++ components/myself-card/background/Background.vue
@@ -0,0 +1,45 @@
+<template>
+	<image class="image-bg" :src="url"/>
+</template>
+
+<script>
+	export default {
+		name:"Background",
+		data() {
+			return {
+			}
+		},
+		props:{
+			url:{
+				type:String,
+				default:""
+			}
+		},
+		components: {
+		},
+		computed: {
+			
+		},
+		onReady(){
+			
+		},
+		onLoad() {
+			
+		},
+		methods: {
+		}
+	}
+</script>
+
+<style>
+	.image-bg {
+	    position: absolute;
+	    z-index: -1;
+	    left: 0;
+	    right: 0;
+	    bottom: 0;
+	    right: 0;
+	    width: 100%;
+	    height: 100%;
+	}
+</style>
--- pages/myself/register/index.vue
+++ pages/myself/register/index.vue
@@ -1,21 +1,34 @@
 <template>
-	<view class="box">
-		
-		<image src="../../../static/myself_background.jpg" mode=""></image>
+	<view class="register_box" :style="{ 'height': register_box_height + 'px' }">
+		<Background :url="backageUrl"></Background>
+		<view class="register_card">
+			
+		</view>
 	</view>
 </template>
 
 <script>
+	import Background from '@/components/myself-card/background/Background.vue'
 	export default {
 		data() {
 			return {
-			
+				backageUrl:"../../../static/myself_background.jpg",
+				register_box_height:0
 			}
 		},
 		components: {
+			Background,
 		},
 		computed: {
 			
+		},
+		onReady(){
+			var that = this;
+			uni.getSystemInfo({
+			    success: function (res) {
+					that.register_box_height = res.windowHeight;
+			    }
+			});
 		},
 		onLoad() {
 			
@@ -26,15 +39,17 @@
 </script>
 
 <style>
-	.box{
-		width: 100%;
-		height: 100%;
-		image{
-			z-index:-1; 
-			width:100%; 
-			height:100%; 
-			position: fixed; 
-			top:0; 
-		}
+	.register_box{
+		display: flex;
+		flex-wrap: nowrap;
+		flex-direction: column-reverse;
+		justify-content: flex-start;
+	}
+	.register_card{
+		order: 1;
+		height: 1200rpx;
+		background-color: rgba(255, 255, 255, 0.83);
+		/* 设置圆角*/
+		border-radius: 50rpx 50rpx ;
 	}
 </style>
